3f3097c4a19a7eeb20555839e3291f38eaa63ca6
Filip Chalupa <chalupa.filip@gmail.com>
2019-11-03 16:14:29 +0100

    Server: handle multiple connections

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import * as express from 'express'
 import * as http from 'http'
 import * as WebSocket from 'ws'
+import * as url from 'url'
+
+console.log('Starting server')
 
 const app = express()
 
@@ -9,15 +12,41 @@ app.use(express.static('src/client'))
 
 const server = http.createServer(app)
 
-const wss = new WebSocket.Server({ server })
+const wsClient = new WebSocket.Server({ noServer: true })
+const wsScreen = new WebSocket.Server({ noServer: true })
+const wsConfig = new WebSocket.Server({ noServer: true })
 
-wss.on('connection', (ws: WebSocket) => {
-	ws.on('message', (message: string) => {
-		console.log('received: %s', message)
-		ws.send(`Hello, you sent -> ${message}`)
-	})
+wsClient.on('connection', (ws: WebSocket) => {
+	console.log('client connection')
+})
 
-	ws.send('Hi there, I am a WebSocket server')
+wsScreen.on('connection', (ws: WebSocket) => {
+	console.log('screen connection')
+})
+
+wsConfig.on('connection', (ws: WebSocket) => {
+	console.log('config connection')
+})
+
+server.on('upgrade', (request, socket, head) => {
+	const pathname = url.parse(request.url).pathname
+	console.log('upgrade')
+
+	if (pathname === '/client') {
+		wsClient.handleUpgrade(request, socket, head, function done(ws) {
+			wsClient.emit('connection', ws, request)
+		})
+	} else if (pathname === '/screen') {
+		wsScreen.handleUpgrade(request, socket, head, function done(ws) {
+			wsScreen.emit('connection', ws, request)
+		})
+	} else if (pathname === '/config') {
+		wsConfig.handleUpgrade(request, socket, head, function done(ws) {
+			wsConfig.emit('connection', ws, request)
+		})
+	} else {
+		socket.destroy()
+	}
 })
 
 server.listen(process.env.PORT || 8999, () => {
