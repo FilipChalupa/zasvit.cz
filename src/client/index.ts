5285e68b4cde42ab988a6fb1366ba8cbca03b1c4
Filip Chalupa <chalupa.filip@gmail.com>
2019-11-03 22:27:45 +0100

    Client: basic layout

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,18 @@
-console.log('Hi')
+const $playground = document.querySelector('.js-playground') as HTMLElement
+const $reflector = document.querySelector('.js-reflector') as HTMLElement
+
+function onMove(event: PointerEvent) {
+	const [x, y] = [event.clientX, event.clientY]
+	$reflector.style.transform = `translate(${x}px, ${y}px)`
+}
+
+$playground.addEventListener('pointerdown', (event) => {
+	$playground.addEventListener('pointermove', onMove)
+	$playground.setPointerCapture(event.pointerId)
+	onMove(event)
+})
+
+$playground.addEventListener('pointerup', (event) => {
+	$playground.removeEventListener('pointermove', onMove)
+	$playground.releasePointerCapture(event.pointerId)
+})
