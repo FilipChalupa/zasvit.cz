0efae99925d0d44f490485609b6d800ac04bc959
Filip Chalupa <chalupa.filip@gmail.com>
2019-11-13 00:53:56 +0100

    Screen: toggling camera notice

--- src/client/screen.ts
+++ src/client/screen.ts
@@ -95,6 +95,21 @@
 			}
 		}
 
+		const toggleFlashWarning = () => {
+			if ($body.classList.contains('is-flashWarning')) {
+				setTimeout(() => {
+					$body.classList.remove('is-flashWarning')
+					toggleFlashWarning()
+				}, 5000)
+			} else {
+				setTimeout(() => {
+					$body.classList.add('is-flashWarning')
+					toggleFlashWarning()
+				}, 14000)
+			}
+		}
+		toggleFlashWarning()
+
 		webSocket.onmessage = (event) => {
 			const data = JSON.parse(event.data)
 			switch (data.command) {
